a8a29795cb8c1bd1160b6def31f1683193ba2fd0
Dawid Maziarz <david9819@onet.eu>
2023-02-03 15:17:22 +0100

    add function to js files

--- js/script.js
+++ js/script.js
@@ -1,46 +1,53 @@
-let inputCash = document.querySelector(".js-cash");
-let inputCurrencyElement = document.querySelector(".js-inputCurrency");
-let outputCurrencyElement = document.querySelector(".js-outputCurrency");
-let resultElement = document.querySelector(".js-result");
-let formElement = document.querySelector(".js-form");
-let resultContainerElement = document.querySelector(".js-resultContainer");
-let form__buttonElement = document.querySelector(".js-form__button");
-
-formElement.addEventListener("submit", (event) => {
-  event.preventDefault();
-  if (resultContainerElement.classList.contains("form__resultContainer")) {
-    resultContainerElement.classList.remove("form__resultContainer");
-  }
-  let currencyPair = `${inputCurrencyElement.value}/${outputCurrencyElement.value}`;
-  let rate;
-
-  switch (currencyPair) {
-    case "PLN/USD":
-      rate = 0.22;
-      break;
-    case "PLN/EUR":
-      rate = 0.21;
-      break;
-    case "USD/PLN":
-      rate = 4.43;
-      break;
-    case "USD/EUR":
-      rate = 0.94;
-      break;
-    case "EUR/PLN":
-      rate = 4.69;
-      break;
-    case "EUR/USD":
-      rate = 1.06;
-      break;
-    default:
-      rate = 1;
-  }
-
-  let amount = inputCash.value;
-  let result = amount * rate;
-
-  resultElement.innerText = `${inputCash.value} ${
-    inputCurrencyElement.value
-  } = ${result.toFixed(2)} ${outputCurrencyElement.value}`;
-});
+{
+  const CalculateCurrency = (currencyPair, amount) => {
+    let rate;
+    switch (currencyPair) {
+      case "PLN/USD":
+        rate = 0.22;
+        return rate * amount;
+      case "PLN/EUR":
+        rate = 0.21;
+        return rate * amount;
+      case "USD/PLN":
+        rate = 4.43;
+        return rate * amount;
+      case "USD/EUR":
+        rate = 0.94;
+        return rate * amount;
+      case "EUR/PLN":
+        rate = 4.69;
+        return rate * amount;
+      case "EUR/USD":
+        rate = 1.06;
+        return rate * amount;
+      default:
+        return 1 * amount;
+    }
+  };
+  const resultUpdate = () => {
+    const resultElement = document.querySelector(".js-result");
+    const inputCash = document.querySelector(".js-cash");
+    const amount = inputCash.value;
+    const inputCurrencyElement = document.querySelector(".js-inputCurrency");
+    const outputCurrencyElement = document.querySelector(".js-outputCurrency");
+    const currencyPair = `${inputCurrencyElement.value}/${outputCurrencyElement.value}`;
+    const result = CalculateCurrency(currencyPair, amount);
+    resultElement.innerText = `${inputCash.value} ${
+      inputCurrencyElement.value
+    } = ${result.toFixed(2)} ${outputCurrencyElement.value}`;
+  };
+  const init = () => {
+    const formElement = document.querySelector(".js-form");
+    const resultContainerElement = document.querySelector(
+      ".js-resultContainer"
+    );
+    formElement.addEventListener("submit", (event) => {
+      event.preventDefault();
+      if (resultContainerElement.classList.contains("form__resultContainer")) {
+        resultContainerElement.classList.remove("form__resultContainer");
+      }
+      resultUpdate();
+    });
+  };
+  init();
+}
